584b745529be432ac9063d8948454928acc4ffcf
DavaArdiansyah <davaardiansyah1404007@gmail.com>
2024-07-18 19:58:13 +0700

    integrasi template

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>@yield('title') - Karier Sebelas</title>
+    @vite(['public/assets/scss/app.scss', 'public/assets/scss/themes/dark/app-dark.scss'])
+    <link rel="shortcut icon" href="{{ asset('assets/static/images/logo/favicon.svg') }}" type="image/x-icon" />
+    <link rel="shortcut icon" href="{{ asset('assets/static/images/logo/favicon.png') }}" type="image/png" />
+    @vite(['public/assets/static/js/initTheme.js', 'public/assets/static/js/components/dark.js', 'public/assets/js/app.js', 'resources/js/perfect-scrollbar.js'])
+    @yield('assets')
+    </head>
+
+    <body>
+        <div id="app">
+            <aside id="sidebar">
+                @include('partials.sidebar')
+            </aside>
+
+            <main id="main">
+                <header class="mb-3">
+                    <a href="#" class="burger-btn d-block d-xl-none">
+                        <i class="bi bi-justify fs-3"></i>
+                    </a>
+                </header>
+
+                <section>
+                    <div class="page-heading">
+                        <h3>@yield('title')</h3>
+                    </div>
+                    <div class="page-content">
+                        @yield('content')
+                    </div>
+                </section>
+
+                @include('partials.footer')
+
+            </main>
+        </div>
+    </body>
+
+    </html>
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,15 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'public/assets/scss/app.scss',
+                'public/assets/scss/themes/dark/app-dark.scss',
+                'public/assets/static/js/initTheme.js',
+                'public/assets/static/js/components/dark.js',
+                'public/assets/js/app.js',
+                'public/assets/scss/iconly.scss',
+                'resources/js/perfect-scrollbar.js',
+            ],
             refresh: true,
         }),
     ],
